b48e0bb9e43b5a7da682ae9767b9035784ecefa2
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2022-12-27 18:49:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Counter from './counter';
 function App() {
   return (
     <>
+    <h1>State management with Zustang</h1>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Counter />
